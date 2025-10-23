d598821b9426ece51bbe6721a71b2980ebfcfe36
mila-cam <m.mariarodriguez2006@gmail.com>
2025-10-23 12:01:26 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-//seleccionar el formulario
+// Seleccionar el formulario
 const formulario = document.querySelector("#contacto")
 
-if(formulario){
-    formulario.addEventListener("submit"),
-    (Event)=>{
-        //evitar que el formulario se envie 
+if (formulario) {
+    formulario.addEventListener("submit",
+        (event) => {
+            //Evitar que el formulario se envie
             event.preventDefault();
 
-            //obtener valores de mi formulario
-            const Nombre=document.getElementById("Nombre").value;
-            const Document=document.getElementById("Comentario").value;
-            const Correo =document.getElementById("Correo").value;
-            const Telefono =document.getElementById("Telefono").value;
+            //Obtener los valores de mi formulario
+            const nombre = document.getElementById("nombre").value;
+            const descripcion = document.getElementById("mensaje").value;
+            const correo = document.getElementById("Correo").value;
+            const numero = document.getElementById("telefono").value;
 
+            //Imprimir los valores por consola
+            console.log("Nombre", nombre)
+            console.log("Correo", correo)
+            console.log("Celular", numero)
+            console.log("Mensaje es", descripcion)
 
-            console.log ("Nombre", Nombre)
-            console.log ("Correo", Correo)
-            console.log ("Telefono", Telefono)
-            console.log ("Comentario es", Document)
-    
             alert("Hola " + nombre +
                 "tu correo es " + correo +
                 "tu motivo de contacto es: " + descripcion)
-        }
+        })
 } else {
     console.error("No se pudo encontrar el formulario con el ID #contacto")
 }
-    
+
+
